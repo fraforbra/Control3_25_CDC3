4f4cee342fadf2a3ffafaed3ae1112cd8f668d4b
fraforbra <fraforbra@alum.us.es>
2025-04-25 11:53:19 +0200

    Error no solucionado

--- src/main/java/is/c3/controller/Comisario.java
+++ src/main/java/is/c3/controller/Comisario.java
@@ -34,7 +34,7 @@ public class Comisario {
 		
 	}
     private void printData() {
-    	System.out.println("Pinturas en la galería:");
+    	System.out.println("Pinturas en la galería: frafrobra");
 		// Mostrar todas las pinturas de la galería
 		System.out.println(galeria.findAll());
     	
